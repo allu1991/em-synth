b2c8756fbef7f89df56542fde9d92467dc54c9d4
allu1991 <jappikakkonen@gmail.com>
2026-05-03 19:05:54 +0300

    Refactor Camera component: remove unused state and animation logic for improved clarity

--- src/Camera.jsx
+++ src/Camera.jsx
@@ -1,60 +1,19 @@
 import { PerspectiveCamera, OrbitControls } from '@react-three/drei'
-import { useState, useRef, useLayoutEffect, useContext, useMemo } from 'react'
+import { useRef, useContext, useMemo } from 'react'
 import { EffectsContext } from './index.jsx'
 
 export default function Camera({ tl }) {
 
+    // Create a ref for the camera and get the free camera state from the context
     const cameraRef = useRef()
     const { isFreeCamera } = useContext(EffectsContext)
 
-    const [cameraAnimDone, setCameraAnimDone] = useState(false);
-
+    // Set the field of view based on the screen (mobile/desktop) for responsiveness
     const fov = useMemo(() => {
         return window.matchMedia('(max-width: 768px)').matches ? 60 : 45;
     }, []);
 
-    // Animate the camera using GSAP timeline
-    // useLayoutEffect(() => {
-    //     if (tl) {
-    //         tl.fromTo(cameraRef.current.rotation, {
-    //             x: 0.3,
-    //             // y: 0.2,
-    //         }, {
-    //             x: 0.0,
-    //             y: 0.0,
-    //             z: 0.0,
-    //             duration: 4,
-    //             ease: "power4.out",
-    //         }).eventCallback("onComplete", () => setCameraAnimDone(true));
-    //     }
-    // }, [ tl ]);
-
     return <>
-
-        {/* {!cameraAnimDone ? (
-            <PerspectiveCamera
-                ref={cameraRef}
-                makeDefault
-                fov={45}
-                near={0.1}
-                far={200}
-                rotation={[0, 0, 0]}
-                position={[0, 0, 7]}
-            />
-        ) : (
-            <OrbitControls
-                enablePan={ true }
-                enableZoom={ true }
-                enableDamping
-                dampingFactor={ 0.05 }
-                rotateSpeed={ 0.7 }
-                minPolarAngle={ Math.PI / 2.5 }
-                maxPolarAngle={ Math.PI / 1.8 }
-                minAzimuthAngle={ -Math.PI / 7.5 }
-                maxAzimuthAngle={ Math.PI / 7.5 }
-            />
-        )} */}
-
         <PerspectiveCamera
             ref={cameraRef}
             makeDefault
